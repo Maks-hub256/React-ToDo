a1bdc21a5ec34b4024c8bb2d128644ae4dc9b186
Maks-hub256 <61085030+Maks-hub256@users.noreply.github.com>
2020-12-27 21:11:31 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [],
+      currentValue: ""
+    }
+  }
+
+  valueChange = (e) => {
+    this.setState({
+      currentValue: e.target.value
+    })
+  }
+
+  addElem = (e) => {
+    e.preventDefault()
+    const list = [...this.state.list]
+    if(this.state.currentValue == "") {
+      return false
+    }
+    list.push(this.state.currentValue)
+    this.setState({
+      list: list,
+      currentValue: ""
+    })
+  }
+
+  onDelete = (index) => {
+    const list = [...this.state.list]
+    list.splice(index, 1)
+    this.setState({
+      list: list
+    })
+  }
+
+
+  render() {
+    return(
+      <div className="wrapper">
+        <form action="" onSubmit={this.addElem}>
+          <input type="text" onChange={this.valueChange} value={this.state.currentValue}/>
+          <input type="submit" value="Add"/>
+        </form>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+              return(
+                <div className="item-wrapper" key={index}>
+                  <li>{item}</li>
+                  <button onClick={this.onDelete.bind(this, index)}>X</button>
+                </div>
+              )
+            })
+  
+          }
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
